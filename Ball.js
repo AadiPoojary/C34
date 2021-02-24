f93b2667265f90684f01f703a52df30b623302d0
Aadi Poojary <75982568+AadiPoojary@users.noreply.github.com>
2021-02-24 15:32:58 +0530

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,29 @@
+class Ball
+{
+    constructor(x,y,r)
+    {
+        var options=
+        {
+            //'restitution':0.8,
+            'frictionAir':0.005,
+            'density':3,
+        
+            
+        }
+
+        this.body=Bodies.circle(x,y,r,options);
+        World.add(world,this.body);
+        this.r=r;
+        //this.image=loadImage("polygon.png")
+    }
+
+    display()
+    {
+        push();
+        fill("yellow");
+        ellipseMode(RADIUS);
+        ellipse(this.body.position.x,this.body.position.y,this.r,this.r);
+        pop();
+    }
+
+}
